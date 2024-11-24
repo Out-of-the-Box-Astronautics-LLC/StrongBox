939297f412b4ca03ae40f27747d498fffee69777
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2024-11-24 06:44:47 -0800

    Fixed unit_test() asserts. Added local debugging toggle, return types, and function documentation

--- KinematicEquations.py
+++ KinematicEquations.py
@@ -23,6 +23,7 @@ from math import pow, sqrt, pi
 ## Internally Developed Library
 import GlobalConstants as GC
 
+LOCAL_DEBUG_STATEMENTS_ON = True
 
 class KinematicEquations:
 
@@ -62,7 +63,8 @@ class KinematicEquations:
         self.eq5 = "dd = 0.5 * (vf + vi) * t"
 
         if sum(unknowns) > 2:
-            if GC.DEBUG_STATEMENTS_ON: print("ERROR: Too many unknowns to calculate the answer")
+            if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON:
+                print("ERROR: Too many unknowns to calculate the answer")
 
         else:
             self.isValid = True
@@ -75,20 +77,24 @@ class KinematicEquations:
             # TODO: What are the 5 combinations of arguments?
             # Calculate Final Velocity in FOUR different ways
             if (unknowns[GC.VF]) and not (unknowns[GC.VI] or unknowns[GC.T] or unknowns[GC.DD] or unknowns[GC.A]):
-                if GC.DEBUG_STATEMENTS_ON: print(f"Using {self.eq4} equation since ONLY final velocity = {velocityFinal} is unknown")
+                if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON: 
+                    print(f"Using {self.eq4} equation since ONLY final velocity = {velocityFinal} is unknown")
                 self.vf = (deltaDistance + (0.5 * acceleration * pow(time, 2))) / time
 
             elif (unknowns[GC.VF] and unknowns[GC.T]) and not (unknowns[GC.VI] or unknowns[GC.DD] or unknowns[GC.A]):
-                if GC.DEBUG_STATEMENTS_ON: print(f"Using {self.eq2} equation since final velocity = {velocityFinal} & time = {time} are unknown")
+                if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON:
+                    print(f"Using {self.eq2} equation since final velocity = {velocityFinal} & time = {time} are unknown")
                 vf_2 = pow(velocityInitial, 2) + (2 * acceleration * deltaDistance)
                 self.vf = sqrt(vf_2)
 
             elif (unknowns[GC.VF] and unknowns[GC.DD]) and not (unknowns[GC.VI] or unknowns[GC.T] or unknowns[GC.A]):
-                if GC.DEBUG_STATEMENTS_ON: print(f"Using {self.eq3} equation since final velocity = {velocityFinal} & delta distance = {deltaDistance} are unknown")
+                if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON:
+                    print(f"Using {self.eq3} equation since final velocity = {velocityFinal} & delta distance = {deltaDistance} are unknown")
                 self.vf = velocityInitial + acceleration * time
 
             elif (unknowns[GC.VF] and unknowns[GC.A]) and not (unknowns[GC.VI] or unknowns[GC.DD] or unknowns[GC.T]):
-                if GC.DEBUG_STATEMENTS_ON: print(f"Using {self.eq5} equation since final velocity = {self.vf} & acceleration = {a} are unknown")
+                if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON:
+                    print(f"Using {self.eq5} equation since final velocity = {self.vf} & acceleration = {a} are unknown")
                 self.vf = ((2 * deltaDistance) / time) - velocityInitial
 
             elif (unknowns[GC.VI] and unknowns[GC.DD]) and not (unknowns[GC.VF] or unknowns[GC.T] or unknowns[GC.A]):
@@ -110,7 +116,8 @@ class KinematicEquations:
 
             # Calculate Time
             elif (unknowns[GC.T] and unknowns[GC.VF]) and not (unknowns[GC.VI] or unknowns[GC.DD] or unknowns[GC.A] or self.acceleration == 0):
-                if GC.DEBUG_STATEMENTS_ON: print(f"Using {self.eq1} equation since ONLY time = {t} is unknown and acceleration does NOT equal 0")
+                if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON:
+                    print(f"Using {self.eq1} equation since ONLY time = {t} is unknown and acceleration does NOT equal 0")
                 t = (sqrt(2 * acceleration * deltaDistance + pow(velocityInitial, 2)) - velocityInitial) / acceleration
 
 
@@ -120,21 +127,22 @@ class KinematicEquations:
                 self.dd = (velocityInitial * time) + (0.5 * acceleration * pow(time, 2))
 
             else:
-                if GC.DEBUG_STATEMENTS_ON: print("WARNING: All arguments have valid known float values, nothing to calculate")
+                if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON: 
+                    print("WARNING: All arguments have valid known float values, nothing to calculate")
 
 
-    def determine_unkwown(vf, vi, t, d, a):
+    def determine_unkwown(vf, vi, t, d, a) -> list:
         """ Determine if input arguments are valid float or interger numbers or a "?" string to be calculated
 
         Arg(s):
-            vf (Float or String):
-            vi (Float or String):
-            t  (Float or String):
-            d  (Float or String):
-            a  (Float or String):
+            vf (Float or String): Final velocity 
+            vi (Float or String): Initial velocity
+            t  (Float or String): Time
+            d  (Float or String): Distance
+            a  (Float or String): Acceleration
 
         Returns:
-            List of Booleans values, based on if an argument is a float (False) or a string (True)
+            List of Booleans values, based on if an input argument is a float (False) or a string (True)
         """
         unknownArguments = [False, False, False, False, False]
 
@@ -166,62 +174,67 @@ class KinematicEquations:
         return unknownArguments
 
 
-    def scalarize_vector(x: float, y: float, z: float):
-        """ Convert 3 dimension vector inside a magnitude without direction
+    def scalarize_vector(x: float, y: float, z: float) -> float:
+        """ Convert a 3 dimensional vector to a magnitude (without direction).
 
+        Arg(s):
+            x (Float): X-axis component of a 3D vector
+            y (Float): Y-axis component of a 3D vector
+            z (Float): Z-axis component of a 3D vector
         """
         scalarVelocity = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
 
         return scalarVelocity
 
 
-    def calculate_potential_energy(mass: float, gravity: float, height: float):
-        """ Calculate the potential energy (PE) an item has
+    def calculate_potential_energy(mass: float, gravity: float, height: float) -> float:
+        """ Calculate the gravitational potential energy (PE) that an item contains.
 
         Arg(s):
-            mass (Float): Mass in kilograms
-            gravity (Float): Acceleration dur to gravity in meters per seconds per second
+            mass (Float): Mass of item in kilograms
+            gravity (Float): Acceleration due to gravity in meters per second per second
             height (Float): Scalar distance in meters above a reference point
         """
         return (mass * gravity * height)
 
 
-    def calculate_kinetic_energy(mass: float, velocity: float):
-        """ Calculate the kinetic energy (KE) an item has
+    def calculate_kinetic_energy(mass: float, velocity: float) -> float:
+        """ Calculate the kinetic energy (KE) from movement that an item contains.
 
         Arg(s):
-            mass (Float): Mass in kilograms
+            mass (Float): Mass of item in kilograms
             velocity (Float): Scalar velocity in meters per second
         """
         return (0.5 * mass * velocity * velocity)
 
 
     def unit_test():
-        """ Checked using the following online calculators
+        """ Verified using the following online calculators:
 	        https://www.satsig.net/orbit-research/orbit-height-and-speed.htm
             https://physicscatalyst.com/calculators/physics/kinematics-calculator.php
             https://study.com/academy/lesson/kinematic-equations-list-calculating-motion.html
         """
-        assert 20 == KinematicEquations.calculate_kinetic_energy(10.0, 2.0), "Error in static calculate_kinetic_energy() function"
+        assert 20.0 == KinematicEquations.calculate_kinetic_energy(10.0, 2.0), "Error in static calculate_kinetic_energy() function"
         assert 33.10875 == KinematicEquations.calculate_potential_energy(1.5, 9.81, 2.25), "Error in static calculate_potential_energy() function"
         
 
         deltaDistance = 111.0 - 0.0
-        answer1 = KinematicEquations("?", 0.0, "?", 111.0, GC.G_EARTH)
+        answer1 = KinematicEquations("?", 0.0, "?", deltaDistance, GC.G_EARTH)
         errorMessage = f"Earth Drop test from 111 meters: Vf = {answer1.vf} | Vi = {answer1.vi} | Time = {answer1.t} | Displacement = {answer1.dd} | Accel = {answer1.a}"
-        assert round(answer1.vf, 3) == GC.TODO, errorMessage
+        assert round(answer1.vf, 3) == 46.667, errorMessage
         
 
-        answer2 = KinematicEquations(44.69, 1633.80, "?", 100_000.0, GC.G_MOON)
+        answer2 = KinematicEquations(44.69, 1633.80, "?", 100000.0, "?")
         errorMessage = f"Moon Burn Time from 100 km: Vf = {answer2.vf} | Vi = {answer2.vi} | Time = {answer2.t} | Displacement = {answer2.dd} | Accel = {answer2.a}"
-        assert round(answer2.t, 3) == GC.TODO, errorMessage
+        assert round(answer2.t, 3) == 119.155, errorMessage
        
 
         xVelocity = 5.03
         yVelocity = 20.10
         zVelocity = 22.56
         scalarInput = KinematicEquations.scalarize_vector(xVelocity, yVelocity, zVelocity)
-        if GC.DEBUG_STATEMENTS_ON: f"Initial Velocity: {scalarInput} m/s = [{xVelocity}, {yVelocity}, {zVelocity}]"
+        if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON: 
+            print(f"Initial Velocity: {scalarInput} m/s = [{xVelocity}, {yVelocity}, {zVelocity}]")
         assert round(scalarInput, 3) == 30.631, "Error in static scalarize_vector() function"
 
 
@@ -232,7 +245,8 @@ class KinematicEquations:
         print(f"Vfy = {yAxis.vf} | Viy = {yAxis.vi} | Time = {yAxis.t} | Y-Axis Displacement = {yAxis.dd} | Y-Axis Accel = {yAxis.a}")
         print(f"Vfz = {zAxis.vf} | Viz = {zAxis.vi} | Time = {zAxis.t} | Z-Axis Displacement = {zAxis.dd} | Z-Axis Accel = {zAxis.a}")
         scalarOutput = KinematicEquations.scalarize_vector(xAxis.vf, yAxis.vf, zAxis.vf)
-        if GC.DEBUG_STATEMENTS_ON: f"Final Velocity: {scalarOutput} m/s = [{xAxis.vf}, {yAxis.vf}, {zAxis.vf}]"
+        if GC.DEBUG_STATEMENTS_ON or LOCAL_DEBUG_STATEMENTS_ON:
+            print(f"Final Velocity: {scalarOutput} m/s = [{xAxis.vf}, {yAxis.vf}, {zAxis.vf}]")
         assert round(scalarOutput, 3) == GC.TODO, "Error in static scalarize_vector() function"
 
 
